10fbb6d4210f2bd76729acfbb33723dcbe7de2ee
firefightingrobot <jssfirefightingrobot@gmail.com>
2025-01-02 23:49:52 +0530

    Automated upload: Thu Jan  2 23:49:52 IST 2025

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/arjuna/catkin_ws/src'
+whitelisted_packages = 'ros_imu_bno055'.split(';') if 'ros_imu_bno055' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/arjuna/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/arjuna/catkin_ws/devel;/opt/ros/melodic' != '' else []
---
+++ catkin_ws/build/firefighter/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/arjuna/catkin_ws/src/firefighter
+# Build directory: /home/arjuna/catkin_ws/build/firefighter
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
